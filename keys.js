a45ef7d8ec5704a591d32dcc8941467654d7afee
Ajay Yadav <ajay.yadav@AWKSOSX014.local>
2019-04-29 15:55:31 -0700

    Got movie-this working

--- keys.js
+++ keys.js
@@ -4,3 +4,6 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+// SPOTIFY_ID=a6a3b1625c17427cb4076df7536fc63f
+// SPOTIFY_SECRET=4deedddcd63046448a9d171c267ba27d
--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
-var request = require("request");
+var axios = require("axios");
 var fs = require("fs");
 var keys = require("./keys");
 
@@ -11,3 +11,58 @@ let secondCommand = process.argv.slice(3).join(" ");
 
 console.log(command);
 console.log(secondCommand);
+
+main(command, secondCommand);
+
+function main(command, secondCommand) {
+    switch (command) {
+        case "spotify-this-song":
+            doSpotifyThings(secondCommand);
+            break;
+        case "movie-this":
+            doMovieThings(secondCommand);
+            break;
+        case "concert-this":
+            doBandThings(secondCommand);
+            break;
+        case "do-what-it-says":
+            doWhatItSays(secondCommand)
+            break;
+        default:
+            runDefault();
+            break;
+    }
+}
+
+function doMovieThings(secondCommand) {
+    var movieURL = "http://www.omdbapi.com/?apikey=trilogy&t=" + secondCommand;
+
+    axios
+        .get(movieURL)
+        .then(function (response) {
+            // If the axios was successful...
+            // Then log the body from the site!
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+function runDefault() {
+
+}
